a6f168f8f3adc6e9e0d161213dac7ea43f84ef40
George Leslie-Waksman <george@cloverhealth.com>
2017-11-28 10:06:14 -0800

    Do it safely

--- src/main/kotlin/com/gwax/DoMath.kt
+++ src/main/kotlin/com/gwax/DoMath.kt
@@ -3,22 +3,17 @@ package com.gwax
 import com.gwax.lang.Arith
 
 fun doMath(lexer: Arith): Double {
-    val vocab = lexer.vocabulary
-    var op = "ADDITION"
+    var op = Arith.ADDITION
     var total = 0.0
-    val tokenPairs = lexer.allTokens.map {
-        t -> Pair(vocab.getSymbolicName(t.type), t.text)
-    }
-    for ((type, text) in tokenPairs) {
-        when (type) {
-            "NUMBER" -> {
-                val value = text.toDouble()
+    lexer.allTokens.forEach {
+        when (it.type) {
+            Arith.NUMBER -> {
                 when (op) {
-                    "ADDITION" -> total += value
-                    "SUBTRACTION" -> total -= value
+                    Arith.ADDITION -> total += it.text.toDouble()
+                    Arith.SUBTRACTION -> total -= it.text.toDouble()
                 }
             }
-            else -> op = type
+            Arith.ADDITION, Arith.SUBTRACTION -> op = it.type
         }
     }
     return total
