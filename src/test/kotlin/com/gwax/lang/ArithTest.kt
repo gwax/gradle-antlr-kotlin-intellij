0008e670ae436e6f7f86a4c1ffb7677e2bb2ca24
George Leslie-Waksman <george@cloverhealth.com>
2017-11-29 11:25:09 -0800

    better error messages

--- src/test/kotlin/com/gwax/lang/ArithTest.kt
+++ src/test/kotlin/com/gwax/lang/ArithTest.kt
@@ -1,25 +1,30 @@
 package com.gwax.lang
 
+import com.gwax.lang.Arith
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.Lexer
+import org.antlr.v4.runtime.Token
 import org.junit.Test
 import kotlin.test.assertEquals
 
 class ArithTest {
     private fun lex(input: String) = Arith(CharStreams.fromString(input))
-
-    private fun tokenize(lexer: Lexer): List<String> {
-        val vocab = lexer.vocabulary
-        return lexer.allTokens.map { t -> vocab.getSymbolicName(t.type) }
+    private fun Lexer.tokenTypes() = this.allTokens.map { t -> t.type }
+    private fun Lexer.symbolicNames(tokenTypes: List<Int>): List<String> = tokenTypes.map {
+        this.vocabulary.getSymbolicName(it)
     }
 
     @Test fun parseAddition() {
-        assertEquals(listOf("NUMBER", "ADDITION", "NUMBER"),
-                tokenize(lex("1+2.4")))
+        val expected = listOf(Arith.NUMBER, Arith.ADDITION, Arith.NUMBER)
+        val lexer = lex("1+2.4")
+        assertEquals(lexer.symbolicNames(expected),
+            lexer.symbolicNames(lexer.tokenTypes()))
     }
 
     @Test fun parseSkipWhitespace() {
-        assertEquals(listOf("NUMBER", "SUBTRACTION", "NUMBER"),
-                tokenize(lex("1 - 2")))
+        val expected = listOf(Arith.NUMBER, Arith.SUBTRACTION, Arith.NUMBER)
+        val lexer = lex("1 - 2")
+        assertEquals(lexer.symbolicNames(expected),
+            lexer.symbolicNames(lexer.tokenTypes()))
     }
 }
